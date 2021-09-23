e51efd509b67fc87a8199714a1b519579648cdcf
Next In <vedgupta@protonmail.com>
2021-09-23 13:38:33 +0000

    Some minor files deleted

--- install.sh
+++
@@ -1,7 +0,0 @@
-# install vs code extension
-code --install-extension "/workspace/PreNotebook/z-comp/formulahendry.auto-rename-tag-0.1.8.vsix" 
-code --install-extension "/workspace/PreNotebook/z-comp/rangav.vscode-thunder-client-1.9.0.vsix"
-
-cd backend 
-npm install --save 
-npm install -D -g nodemon

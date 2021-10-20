2dd98287fa2bfd95f8ed0beb13af48e797791d71
Ved Gupta <vedgupta@protonmail.com>
2021-10-20 20:11:05 +0530

    react app created and navbar template addedd

---
+++ prenotebook/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Component/Navbar'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App font-link">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;

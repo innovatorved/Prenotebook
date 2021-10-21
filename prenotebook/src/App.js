9c401e4f07ecf96abac116c3bbb554e85ad6510c
Ved Gupta <vedgupta@protonmail.com>
2021-10-21 11:26:03 +0530

    all data transfer to gitpod workspace

--- prenotebook/src/App.js
+++ prenotebook/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
 import Navbar from './Component/Navbar'
 import './App.css'
+import Login from "./Component/Login";
 
 function App() {
   return (
     <div className="App font-link">
       <Navbar/>
+    <Router>
+      <Route exact path="/login">
+        <Login/>
+      </Route>
+    </Router>
     </div>
   );
 }
---
+++ prenotebook/src/Component/Login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="container">
+            <form>
+            <div className="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+            </div>
+            <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}

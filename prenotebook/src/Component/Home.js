2cb7dd501f025f3f4ba358345aa6d5550f692c36
Ved Gupta <vedgupta@protonmail.com>
2021-11-11 23:49:09 +0530

    to gitpod

--- prenotebook/src/Component/Home.js
+++ prenotebook/src/Component/Home.js
@@ -3,7 +3,28 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            <h1>Add a Note</h1>
+            <div className="container my-3">
+            <h2>Add a Note</h2>
+            <form>
+                <div className="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <small id="emailHelp" class ="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label class ="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            </div>
+            <div className="container my-3">
+                <h2>Your Notes</h2>
+            </div>
             
         </div>
     )

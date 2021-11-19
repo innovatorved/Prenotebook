0994656ed8318268044af82acde1112f5ee69e64
Next In <vedgupta@protonmail.com>
2021-11-19 13:03:57 +0000

    new update in mode mode state save in local storage

--- prenotebook/src/Context/notes/BackState.js
+++ prenotebook/src/Context/notes/BackState.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState ,useEffect} from 'react';
 
 // Createcontext
 const BackContext = createContext();
@@ -7,6 +7,20 @@ const BackContext = createContext();
 const BackState = (props) => {
 
     const [mode, setmode] = useState("light");
+    useEffect(() => {
+        if(localStorage.getItem("mode")){
+            if (localStorage.getItem("mode") !== "light"){
+                setmode("dark");
+                localStorage.setItem("mode" , "dark");
+                document.body.style.backgroundColor = "#32383e";
+            }else{
+                setmode("light");
+                localStorage.setItem("mode" , "light");
+                document.body.style.backgroundColor = "#eef2e4";
+            }
+        }
+    }, [])
+
     const [search, setsearch] = useState("");
 
     const ChangeSearch=(value)=>{
@@ -17,9 +31,11 @@ const BackState = (props) => {
         // document.body.style.backgroundColor = mode==="light"?"#eef2e4":"#32383e;";
         if (mode === "light"){
             setmode("dark");
+            localStorage.setItem("mode" , "dark");
             document.body.style.backgroundColor = "#32383e";
         }else{
             setmode("light");
+            localStorage.setItem("mode" , "light");
             document.body.style.backgroundColor = "#eef2e4";
         }
     };

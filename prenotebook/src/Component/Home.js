2cf16cb17bc77f04baf223fd3a45939c1b35a2a3
Next In <vedgupta@protonmail.com>
2022-01-08 14:02:18 +0000

    unused dependecies removed

--- prenotebook/src/Component/Home.js
+++ prenotebook/src/Component/Home.js
@@ -1,4 +1,5 @@
-import React , {useEffect , useState} from 'react';
+import React from 'react';
+// import {useEffect , useState} from 'react';
 // import HomePage from './HomePage';
 import Notes from './Notes';
 

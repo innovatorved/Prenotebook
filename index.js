1c8bd53e041a040e466cc11f2477a93dc2760c61
Ved Gupta <vedgupta@protonmail.com>
2024-10-17 16:24:54 +0000

    updated

--- index.js
+++ index.js
@@ -10,16 +10,12 @@ connectToMongo();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.use(morgan("combined"));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
-
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-});
 
 app.listen(port, () => {
   console.log(`PreNotebook app listening at https://localhost:${port}/`);

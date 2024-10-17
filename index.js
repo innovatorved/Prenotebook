30fd0bf87b9127250a08faa3016626af48e70699
Ved Gupta <vedgupta@protonmail.com>
2024-10-17 16:32:45 +0000

    add build

--- index.js
+++ index.js
@@ -8,14 +8,21 @@ const morgan = require("morgan");
 connectToMongo();
 
 const app = express();
+const reactRouter = express.Router();
+const buildPath = path.normalize(path.join(__dirname, "./frontend/build"))
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.use(morgan("combined"));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+
+reactRouter.get('(/*)?', async (req, res, next) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+})
+app.use(reactRouter)
+
 
 app.listen(port, () => {
   console.log(`PreNotebook app listening at https://localhost:${port}/`);
